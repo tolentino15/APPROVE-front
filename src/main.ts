6aea490386e1abbbdc03bab3b0f70a044ecf44cc
LuizTolentino <luiztm10@gmail.com>
2025-09-24 18:23:46 -0300

    FRONT-50.01-Esqueleto e funcionalidade dos botôes para diferentes páginas

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from './App.vue';
-import router from './router';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
 
 // Vuetify imports
-import { createVuetify } from 'vuetify';
-import 'vuetify/styles';
-import { aliases, mdi } from 'vuetify/iconsets/mdi';
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 
 const vuetify = createVuetify({
-	icons: {
-		defaultSet: 'mdi',
-		aliases,
-		sets: { mdi },
-	},
-});
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: { mdi },
+  },
+})
 
-const app = createApp(App);
-app.use(createPinia());
-app.use(router);
-app.use(vuetify);
-app.mount('#app');
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
