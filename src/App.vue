9bb587378888294c48f7faef343df183e0298602
LuizTolentino <luiztm10@gmail.com>
2025-09-23 21:50:19 -0300

    Projeto Base Front

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+function showAlert() {
+  window.alert('Botão Vuetify!')
+}
+</script>
+
+<template>
+  <v-app>
+    <v-btn color="primary" @click="showAlert">Botão Vuetify</v-btn>
+    <v-main>
+      <v-container class="py-4">
+        <h1>Vue + Vuetify funcionando!</h1>
+        <v-btn color="primary" @click="showAlert">Botão Vuetify</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped></style>
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
+
+// Vuetify imports
+import { createVuetify } from 'vuetify';
+import 'vuetify/styles';
+import { aliases, mdi } from 'vuetify/iconsets/mdi';
+
+const vuetify = createVuetify({
+	icons: {
+		defaultSet: 'mdi',
+		aliases,
+		sets: { mdi },
+	},
+});
+
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+app.use(vuetify);
+app.mount('#app');
